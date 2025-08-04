3551c2c2c7a46bcdcd175f97d07c69d82536d188
Hex <theinferos@gmail.com>
2025-08-04 12:02:53 +0100

    Added Search for individual user

--- backend/src/main/java/com/stream_app/toolkit/repositories/ViewerRepository.java
+++ backend/src/main/java/com/stream_app/toolkit/repositories/ViewerRepository.java
@@ -1,6 +1,7 @@
 package com.stream_app.toolkit.repositories;
 
 import com.stream_app.toolkit.entities.Viewer;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -15,5 +16,8 @@ public interface ViewerRepository extends JpaRepository<Viewer, UUID>{
 
     @Query(value = "SELECT * FROM viewer", nativeQuery = true)
     List<Viewer> getAllViewers();
+
+    @Query(value = "SELECT * FROM viewer WHERE id = :id", nativeQuery = true)
+    Viewer getViewer(@Param("id") UUID id);
 
 }
